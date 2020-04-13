b2de566f6db509556c228ca0f2541ab3867ada56
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-04-13 13:42:32 +0200

    extract getBookListWithOwners

--- src/event_listener.ts
+++ src/event_listener.ts
@@ -1,36 +1,12 @@
 import { App } from "@slack/bolt";
-import { setHomeView, updateHomeView } from "./home_view";
-import { getBookList } from "./db/book_repository";
-import { getUserDetails } from "./slack_user_repository";
-import { getUserIdFromAppHomeEvent } from "./utils/slack_utils";
+import { getBookListWithOwners } from "./services/update_view_with_booklist";
 
 export const setEventListeners = (app: App) => {
   app.event("app_home_opened", async ({ event, context, body }) => {
     try {
-      const { view } = await setHomeView(app, context.botToken, event.user);
-
-      const books = await getBookList();
-
-      const booksWithOwners = books.map(async (book) => {
-        const owners = book.owners.map(async (owner: string) => {
-          return getUserDetails(app, context.botToken, owner);
-        });
-
-        const userId = getUserIdFromAppHomeEvent(body);
-
-        return {
-          ...book,
-          owners: await Promise.all(owners),
-          isCreator: userId === book.createdBy,
-        };
-      });
-
-      updateHomeView(
-        app,
-        context.botToken,
-        (view as any).id,
-        await Promise.all(booksWithOwners),
-      );
+      if (event.view) {
+        await getBookListWithOwners(app, context, body, (event.view as any).id);
+      }
     } catch (error) {
       console.error(error);
     }
---
+++ src/services/update_view_with_booklist.ts
@@ -0,0 +1,36 @@
+import { App } from "@slack/bolt";
+
+import { getBookList } from "../db/book_repository";
+import { getUserDetails } from "../slack_user_repository";
+import { getUserIdFromAppHomeEvent } from "../utils/slack_utils";
+import { updateHomeView } from "../home_view";
+
+export const getBookListWithOwners = async (
+  app: App,
+  context: any,
+  body: any,
+  viewId: string,
+) => {
+  const books = await getBookList();
+
+  const booksWithOwners = books.map(async (book) => {
+    const owners = book.owners.map(async (owner: string) => {
+      return getUserDetails(app, context.botToken, owner);
+    });
+
+    const userId = getUserIdFromAppHomeEvent(body);
+
+    return {
+      ...book,
+      owners: await Promise.all(owners),
+      isCreator: userId === book.createdBy,
+    };
+  });
+
+  updateHomeView(
+    app,
+    context.botToken,
+    viewId,
+    await Promise.all(booksWithOwners),
+  );
+};
