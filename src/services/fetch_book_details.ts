d1ffb0f79b3822f1d67f7c7fd845cbdd6218af7c
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-04-18 16:37:51 +0200

    fetch description

--- src/services/fetch_book_details.ts
+++ src/services/fetch_book_details.ts
@@ -13,36 +13,64 @@ export function fetchBookDetails(url: string) {
     })
     .then(async (browser) => {
       const page = await browser.newPage();
-      await page.goto(url);
 
+      await page.goto(url);
       await page.waitForSelector("body");
 
-      const productInfo = await page.evaluate(() => {
-        const title = (document.body.querySelector("#productTitle") as any)
-          .innerText;
-
-        const ratingElement = document.body
-          .querySelector(".a-icon.a-icon-star")
-          .getAttribute("class");
-        const integer = ratingElement.replace(/[^0-9]/g, "").trim();
-        const parsedRating = parseInt(integer) / 10;
-
-        const image = document.body
-          .querySelector("#imgBlkFront")
-          .getAttribute("src");
-
-        return {
-          title: title,
-          rating: parsedRating,
-          image,
-        };
-      });
+      const productInfo = await evaluateBook(page);
+      const bookDescription = await evaluateBookDescription(page);
 
       await browser.close();
 
-      return productInfo;
+      return { ...productInfo, ...bookDescription };
     })
     .catch(function (error) {
       console.error(error);
     });
+}
+
+async function evaluateBook(page: puppeteer.Page) {
+  return await page.evaluate(() => {
+    const title = (document.body.querySelector("#productTitle") as any)
+      .innerText;
+
+    const ratingElement = document.body
+      .querySelector(".a-icon.a-icon-star")
+      .getAttribute("class");
+    const integer = ratingElement.replace(/[^0-9]/g, "").trim();
+    const parsedRating = parseInt(integer) / 10;
+
+    const image = document.body
+      .querySelector("#imgBlkFront")
+      .getAttribute("src");
+
+    return {
+      title: title,
+      rating: parsedRating,
+      image,
+    };
+  });
+}
+
+async function evaluateBookDescription(page: puppeteer.Page) {
+  const descriptionIframe = page
+    .frames()
+    .find((frame) => frame.name().includes("bookDesc_iframe"));
+
+  if (descriptionIframe) {
+    const result = await descriptionIframe.evaluate(() => {
+      const description = (document.body.querySelector("#iframeContent") as any)
+        .innerText;
+
+      return {
+        description,
+      };
+    });
+
+    return result;
+  }
+
+  return {
+    description: "",
+  };
 }
